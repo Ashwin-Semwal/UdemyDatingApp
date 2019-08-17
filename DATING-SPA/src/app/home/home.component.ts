978cfc4711460fc415cea68bbb2e1bf0da7689e8
Ashwin <ashwin.semwal5555@gmail.com>
2019-08-17 13:24:01 +0530

    HomeRegister UI : INput OutPut Emits

--- DATING-SPA/src/app/home/home.component.ts
+++ DATING-SPA/src/app/home/home.component.ts
@@ -2,17 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-value',
-  templateUrl: './value.component.html',
-  styleUrls: ['./value.component.css']
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
-export class ValueComponent implements OnInit {
+export class HomeComponent implements OnInit {
+  registerMode = false;
   values: any;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
     this.getValue();
+  }
+
+  toggleRegisterMode() {
+    this.registerMode = !this.registerMode;
   }
 
   getValue() {
@@ -25,5 +30,9 @@ export class ValueComponent implements OnInit {
         console.log('Error');
       }
     );
+  }
+
+  cancelRegisterMode(registerMode: boolean) {
+    this.registerMode = false;
   }
 }
